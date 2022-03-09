fee478688b40b7bce58e5f8f1abfafe2a07c4a01
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-08 20:25:37 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+    console.log(items);
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((x, index) => (
+            <ToDoItem key={index} item={x} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function ToDoItem(props) {
+  const newItem = props.item;
+  const itemKey = props.key;
+
+  const [isClicked, setIsClicked] = useState("");
+
+  function strikeThrough() {
+    if (isClicked !== "line-through") {
+      setIsClicked("line-through");
+    } else if (isClicked === "line-through") {
+      setIsClicked("");
+    }
+  }
+
+  return (
+    //style={{ textDecorationLine: 'line-through' }}
+    <li
+      style={{ textDecorationLine: isClicked }}
+      onClick={strikeThrough}
+      key={itemKey}
+    >
+      {newItem}
+    </li>
+  );
+}
+
+export default ToDoItem;
